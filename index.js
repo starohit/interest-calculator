13a627edf719f95a16d1908914ad30def2da7a40
Rohit Singh <77211013+starohit@users.noreply.github.com>
2023-12-05 21:09:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function calculate(){
+    const totalAmount = document.getElementById("total-amount");
+    const principalInput = document.getElementById("principal");
+    const rateInput = document.getElementById("rate");
+    const yearsInput = document.getElementById("years");
+
+    let principal = Number(principalInput.value);
+    let rate = Number(rateInput.value/100);
+    let years = Number(yearsInput.value);
+
+    const result = principal * Math.pow((1+rate/1),1*years);
+
+    totalAmount.textContent = result.toLocaleString(undefined,
+                                                            {
+                                                                style:"currency",
+                                                                currency:"USD",
+                                                            });
+}
